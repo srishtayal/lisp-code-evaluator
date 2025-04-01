38ab4f19f3aae646bbfe0fcdf6434285e46f71ef
Srishti Tayal <168971159+srishtayal@users.noreply.github.com>
2025-04-01 22:52:11 +0530

    update index.ts

--- server/index.ts
+++ server/index.ts
@@ -59,12 +59,13 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
+  const port = process.env.PORT || 5000; // Use Railway-assigned port
   server.listen({
     port,
     host: "0.0.0.0",
     reusePort: true,
   }, () => {
-    log(`serving on port ${port}`);
+    log(`serving on port ${port}`); 
   });
+
 })();
